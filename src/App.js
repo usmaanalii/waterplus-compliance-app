cbbee2eabfb2a2256bf12a3bc64410d9ce681514
USMAAN <usmaan@USMAANs-MacBook-Pro.local>
2022-02-20 21:57:40 +0000

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const defaultCheckedItemsState = {
+    listA: 0,
+    listB: 0,
+  };
+  const [checkedItems, setCheckedItems] = useState(defaultCheckedItemsState);
+
+  const [compliancePassed, setCompliancePassed] = useState(false);
+
+  const onChangeHandler = (event) => {
+    if (event.target.checked) {
+      setCheckedItems({
+        ...checkedItems,
+        [event.target.name]: (checkedItems[[event.target.name]] += 1),
+      });
+    } else {
+      setCheckedItems({
+        ...checkedItems,
+        [event.target.name]: (checkedItems[[event.target.name]] -= 1),
+      });
+    }
+
+    if (
+      checkedItems.listA >= 3 ||
+      (checkedItems.listA >= 2 && checkedItems.listB >= 2)
+    ) {
+      return setCompliancePassed(true);
+    } else {
+      return setCompliancePassed(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ fontSize: "16px", textDecoration: "underline" }}>
+        3 Question from List A or 2 Questions from List A and 2 Questions from
+        List B
+      </h1>
+
+      <h2 style={{ color: compliancePassed ? "green" : "red" }}>
+        {compliancePassed
+          ? "Compliance status: PASSED"
+          : "Compliance status: FAILED"}
+      </h2>
+
+      <div>
+        <h2>List A</h2>
+
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="a_1"
+                  name="listA"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="a_1">
+                  Contact Name or any other name authorised on the account
+                  (Including position at company)
+                </label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="a_2"
+                  name="listA"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="a_2">Customer’s Account Number</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="a_3"
+                  name="listA"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="a_3">Amount of Last Invoice</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="a_4"
+                  name="listA"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+                <label htmlFor="a_4">Customer’s current Payment Method</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="a_3"
+                  name="listA"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+                <label htmlFor="a_3">Customer’s Email Address</label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h2>List B</h2>
+
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="b_1"
+                  name="listB"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="b_1">Company Name/Trading Name</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="b_2"
+                  name="listB"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="b_2">Customer’s Contact Number</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="b_3"
+                  name="listB"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="b_3">Customer’s Billing Address</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  id="b_4"
+                  name="listB"
+                  onChange={(event) => onChangeHandler(event)}
+                />
+
+                <label htmlFor="b_4">Customer’s Supply Address</label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <br />
+
+      <button
+        onClick={() => {
+          setCheckedItems(defaultCheckedItemsState);
+          setCompliancePassed(false);
+
+          return document
+            .querySelectorAll("input[type=checkbox]")
+            .forEach((el) => (el.checked = false));
+        }}
+      >
+        Reset
+      </button>
+    </>
   );
-}
+};
 
 export default App;
